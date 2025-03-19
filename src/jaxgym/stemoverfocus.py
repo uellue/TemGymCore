3be567e0aead749b0637f6102a4601c943f040c9
DavidLanders95 <davidlanders2009@hotmail.com>
2025-03-19 12:59:53 +0100

    unjaxing the unneccessary parts

--- src/jaxgym/stemoverfocus.py
+++ src/jaxgym/stemoverfocus.py
@@ -1,151 +1,11 @@
+import numpy as np
 import jax
 import jax.numpy as jnp
 from .ray import Ray
 from . import components as comp
 from .run import run_model_for_jacobians
 
-@jax.jit
-def map_px_on_detector_to_scan(ScanGrid, Detector, detector_image,
-                               ray_scan_coords_x, ray_scan_coords_y,
-                               ray_det_coords_x, ray_det_coords_y):
-    # When propgating backwards we need the negative of the scan rotation to be applied to the ray scan coordinates,
-    # so that we can index pixels correctly on the scan grid - essentially we are rotating 
-    # back into the coordinate system of the scan grid, so that a pixel at (0, 0) in the scan grid
-    # represents the top left corner of the scan grid.
-    
-    scan_rotation = -ScanGrid.scan_rotation
-    scan_step = ScanGrid.scan_step
-    scan_shape = ScanGrid.scan_shape
-    det_shape = Detector.shape
-    det_pixel_size = Detector.pixel_size
-
-    # Rotate the scan coordinates.
-    scan_rotation_rad = jnp.deg2rad(scan_rotation)
-    ray_scan_coords_y_rot = jnp.cos(scan_rotation_rad) * ray_scan_coords_y - jnp.sin(scan_rotation_rad) * ray_scan_coords_x
-    ray_scan_coords_x_rot = jnp.sin(scan_rotation_rad) * ray_scan_coords_y + jnp.cos(scan_rotation_rad) * ray_scan_coords_x
-
-    # Convert rotated scan coordinates into pixel indices.
-    scan_pixel_xs = jnp.round(ray_scan_coords_x_rot / scan_step[1] + scan_shape[1] / 2).astype(jnp.int32)
-    scan_pixel_ys = jnp.round(ray_scan_coords_y_rot / scan_step[0] + scan_shape[0] / 2).astype(jnp.int32)
-
-    # Create a mask of indices within bounds.
-    mask = (scan_pixel_xs >= 0) & (scan_pixel_xs < scan_shape[1]) & \
-           (scan_pixel_ys >= 0) & (scan_pixel_ys < scan_shape[0])
-    
-    det_pixel_xs = jnp.round(ray_det_coords_x / det_pixel_size + det_shape[1] / 2).astype(jnp.int32)
-    det_pixel_ys = jnp.round(ray_det_coords_y / det_pixel_size + det_shape[0] / 2).astype(jnp.int32)
-
-    detector_vals = detector_image[det_pixel_ys, det_pixel_xs]
-
-    scan_pixel_xs = jnp.where(mask, scan_pixel_xs, 0)
-    scan_pixel_ys = jnp.where(mask, scan_pixel_ys, 0)
-    detector_vals = jnp.where(mask, detector_vals, 0)
-    
-    return scan_pixel_xs, scan_pixel_ys, detector_vals
-
-
-@jax.jit
-def map_px_on_scan_to_detector(Detector, sample_interpolant, 
-                               ray_scan_coords_x, ray_scan_coords_y,
-                               ray_det_coords_x, ray_det_coords_y):
-    
-    det_pixel_size = Detector.pixel_size
-    det_shape = Detector.shape
-
-    # Stack the scan coordinates.
-    scan_pts = jnp.stack([ray_scan_coords_y, ray_scan_coords_x], axis=-1)
-    sample_vals = sample_interpolant(scan_pts, )
-    
-    det_pixel_xs = jnp.round(ray_det_coords_x / det_pixel_size + det_shape[1] / 2).astype(jnp.int32)
-    det_pixel_ys = jnp.round(ray_det_coords_y / det_pixel_size + det_shape[0] / 2).astype(jnp.int32)
-    
-    return det_pixel_xs, det_pixel_ys, sample_vals
-
-
-@jax.jit
-def run_model_for_rays_and_slopes(transfer_matrices, input_slopes, scan_position):
-    # Given an input fourdstem model and its transfer matrix, run the model to find the positions of the rays at the sample and detector
-    # from a starting point source with a certain scan position.
-    scan_pos_y, scan_pos_x = scan_position
-
-    input_slopes_x = input_slopes[0]
-    input_slopes_y = input_slopes[1]
-
-    # Make the input rays we can run through one last time in the model to find positions at sample and detector
-    rays_at_source_with_semi_conv = jnp.vstack([
-        jnp.full((input_slopes_x.shape[0],), scan_pos_x),
-        jnp.full((input_slopes_y.shape[0],), scan_pos_y),
-        input_slopes_x,
-        input_slopes_y,
-        jnp.ones_like(input_slopes_x)
-    ])
-
-    # Propagate the point source coordinates through the forward ABCD matrices
-    coord_list = [rays_at_source_with_semi_conv]
-    for ABCD in transfer_matrices:
-        new_coord = jnp.dot(ABCD, coord_list[-1])
-        coord_list.append(new_coord)
-        
-    # Stack the propagated coordinates into an array for easier indexing
-    coords_array = jnp.stack(coord_list, axis=0)
-    
-    xs = coords_array[:, 0, :]
-    ys = coords_array[:, 1, :]
-    dxs = coords_array[:, 2, :]
-    dys = coords_array[:, 3, :]
-
-    coords = jnp.array([xs, ys, dxs, dys])
-    
-    return coords
-
-
-@jax.jit
-def find_input_slopes_that_hit_detpx_from_pt_source_with_semiconv(
-    pixel_coords, scan_pos, semi_conv, transformation_matrix
-):
-    """
-    Given a set of detector pixel coordinates, a semi-convergence angle from a source, and a transformation matrix,
-    find a mask that tells us what slopes will hit the detector pixels from the point source.
-    """
-    scan_pos_y, scan_pos_x = scan_pos
-
-    A_xx, A_xy, B_xx, B_xy = transformation_matrix[0, :4]  # Select first row excluding the last column
-    A_yx, A_yy, B_yx, B_yy = transformation_matrix[1, :4]  # Select second row excluding the last column
-
-    delta_x, delta_y = transformation_matrix[0, 4], transformation_matrix[1, 4]
-
-    y_out, x_out = pixel_coords[:, 0], pixel_coords[:, 1]
-
-    denom = B_xx * B_yy - B_xy * B_yx
-    theta_x_in = (
-        -A_xx * B_yy * scan_pos_x
-        - A_xy * B_yy * scan_pos_y
-        + A_yx * B_xy * scan_pos_x
-        + A_yy * B_xy * scan_pos_y
-        + B_xy * delta_y
-        - B_xy * y_out
-        - B_yy * delta_x
-        + B_yy * x_out
-    ) / denom
-
-    theta_y_in = (
-        A_xx * B_yx * scan_pos_x
-        + A_xy * B_yx * scan_pos_y
-        - A_yx * B_xx * scan_pos_x
-        - A_yy * B_xx * scan_pos_y
-        - B_xx * delta_y
-        + B_xx * y_out
-        + B_yx * delta_x
-        - B_yx * x_out
-    ) / denom
-
-    F = (theta_x_in**2 + theta_y_in**2) - semi_conv**2
-    mask = F <= 0
-    input_slopes_masked = jnp.stack([theta_x_in, theta_y_in]) * mask
-    
-    return input_slopes_masked
-
-
+# This is the only function where it's unavoidable to use jax, as we are getting the jacobian of the model
 @jax.jit
 def get_transfer_matrices(model, scan_pos_m):
 
@@ -196,7 +56,141 @@ def get_transfer_matrices(model, scan_pos_m):
 
     return transfer_matrices, total_transfer_matrix, detector_to_sample
 
-@jax.jit
+
+def map_px_on_detector_to_scan(ScanGrid, Detector, detector_image,
+                               ray_scan_coords_x, ray_scan_coords_y,
+                               ray_det_coords_x, ray_det_coords_y):
+    # When propagating backwards we need the negative of the scan rotation to be applied to the ray scan coordinates,
+    # so that we can index pixels correctly on the scan grid - essentially we are rotating 
+    # back into the coordinate system of the scan grid, so that a pixel at (0, 0) in the scan grid
+    # represents the top left corner of the scan grid.
+    
+    scan_rotation = -ScanGrid.scan_rotation
+    scan_step = ScanGrid.scan_step
+    scan_shape = ScanGrid.scan_shape
+    det_shape = Detector.shape
+    det_pixel_size = Detector.pixel_size
+
+    # Rotate the scan coordinates.
+    scan_rotation_rad = np.deg2rad(scan_rotation)
+    ray_scan_coords_y_rot = np.cos(scan_rotation_rad) * ray_scan_coords_y - np.sin(scan_rotation_rad) * ray_scan_coords_x
+    ray_scan_coords_x_rot = np.sin(scan_rotation_rad) * ray_scan_coords_y + np.cos(scan_rotation_rad) * ray_scan_coords_x
+
+    # Convert rotated scan coordinates into pixel indices.
+    scan_pixel_xs = np.round(ray_scan_coords_x_rot / scan_step[1] + scan_shape[1] / 2).astype(np.int32)
+    scan_pixel_ys = np.round(ray_scan_coords_y_rot / scan_step[0] + scan_shape[0] / 2).astype(np.int32)
+
+    # Create a mask of indices within bounds.
+    mask = (scan_pixel_xs >= 0) & (scan_pixel_xs < scan_shape[1]) & \
+           (scan_pixel_ys >= 0) & (scan_pixel_ys < scan_shape[0])
+    
+    det_pixel_xs = np.round(ray_det_coords_x / det_pixel_size + det_shape[1] / 2).astype(np.int32)
+    det_pixel_ys = np.round(ray_det_coords_y / det_pixel_size + det_shape[0] / 2).astype(np.int32)
+
+    detector_vals = detector_image[det_pixel_ys, det_pixel_xs]
+    
+    return scan_pixel_xs[mask], scan_pixel_ys[mask], detector_vals[mask]
+
+
+def map_px_on_scan_to_detector(Detector, sample_interpolant, 
+                               ray_scan_coords_x, ray_scan_coords_y,
+                               ray_det_coords_x, ray_det_coords_y):
+    
+    det_pixel_size = Detector.pixel_size
+    det_shape = Detector.shape
+
+    # Stack the scan coordinates.
+    scan_pts = np.stack([ray_scan_coords_y, ray_scan_coords_x], axis=-1)
+    sample_vals = sample_interpolant(scan_pts)
+    
+    det_pixel_xs = np.round(ray_det_coords_x / det_pixel_size + det_shape[1] / 2).astype(np.int32)
+    det_pixel_ys = np.round(ray_det_coords_y / det_pixel_size + det_shape[0] / 2).astype(np.int32)
+    
+    return det_pixel_xs, det_pixel_ys, sample_vals
+
+
+def run_model_for_rays_and_slopes(transfer_matrices, input_slopes, scan_position):
+    # Given an input fourdstem model and its transfer matrix, run the model to find the positions of the rays at the sample and detector
+    # from a starting point source with a certain scan position.
+    scan_pos_y, scan_pos_x = scan_position
+
+    input_slopes_x = input_slopes[0]
+    input_slopes_y = input_slopes[1]
+
+    # Make the input rays we can run through one last time in the model to find positions at sample and detector
+    rays_at_source_with_semi_conv = np.vstack([
+        np.full((input_slopes_x.shape[0],), scan_pos_x),
+        np.full((input_slopes_y.shape[0],), scan_pos_y),
+        input_slopes_x,
+        input_slopes_y,
+        np.ones_like(input_slopes_x)
+    ])
+
+    # Propagate the point source coordinates through the forward ABCD matrices
+    coord_list = [rays_at_source_with_semi_conv]
+    for ABCD in transfer_matrices:
+        new_coord = np.dot(ABCD, coord_list[-1])
+        coord_list.append(new_coord)
+        
+    # Stack the propagated coordinates into an array for easier indexing
+    coords_array = np.stack(coord_list, axis=0)
+    
+    xs = coords_array[:, 0, :]
+    ys = coords_array[:, 1, :]
+    dxs = coords_array[:, 2, :]
+    dys = coords_array[:, 3, :]
+
+    coords = np.array([xs, ys, dxs, dys])
+    
+    return coords
+
+
+def find_input_slopes_that_hit_detpx_from_pt_source_with_semiconv(
+    pixel_coords, scan_pos, semi_conv, transformation_matrix
+):
+    """
+    Given a set of detector pixel coordinates, a semi-convergence angle from a source, and a transformation matrix,
+    find a mask that tells us what slopes will hit the detector pixels from the point source.
+    """
+    scan_pos_y, scan_pos_x = scan_pos
+
+    A_xx, A_xy, B_xx, B_xy = transformation_matrix[0, :4]  # Select first row excluding the last column
+    A_yx, A_yy, B_yx, B_yy = transformation_matrix[1, :4]  # Select second row excluding the last column
+
+    delta_x, delta_y = transformation_matrix[0, 4], transformation_matrix[1, 4]
+
+    y_out, x_out = pixel_coords[:, 0], pixel_coords[:, 1]
+
+    denom = B_xx * B_yy - B_xy * B_yx
+    theta_x_in = (
+        -A_xx * B_yy * scan_pos_x
+        - A_xy * B_yy * scan_pos_y
+        + A_yx * B_xy * scan_pos_x
+        + A_yy * B_xy * scan_pos_y
+        + B_xy * delta_y
+        - B_xy * y_out
+        - B_yy * delta_x
+        + B_yy * x_out
+    ) / denom
+
+    theta_y_in = (
+        A_xx * B_yx * scan_pos_x
+        + A_xy * B_yx * scan_pos_y
+        - A_yx * B_xx * scan_pos_x
+        - A_yy * B_xx * scan_pos_y
+        - B_xx * delta_y
+        + B_xx * y_out
+        + B_yx * delta_x
+        - B_yx * x_out
+    ) / denom
+
+    F = (theta_x_in**2 + theta_y_in**2) - semi_conv**2
+    mask = F <= 0
+    input_slopes_masked = np.stack([theta_x_in, theta_y_in]) * mask
+    
+    return input_slopes_masked
+
+
 def get_ray_coords_at_scan_and_det(model, scan_pos, total_transfer_matrix, transfer_matrices, detector_to_sample):
     
     PointSource = model[0]
@@ -214,8 +208,8 @@ def get_ray_coords_at_scan_and_det(model, scan_pos, total_transfer_matrix, trans
 
     # Stack coordinates and perform the inverse matrix multiplication to get the sample coordinates
     xs, ys, dxs, dys = coords
-    detector_rays = jnp.stack([xs[-1], ys[-1], dxs[-1], dys[-1], jnp.ones_like(xs[-1])])
-    sample_rays = jnp.dot(detector_to_sample, detector_rays)
+    detector_rays = np.stack([xs[-1], ys[-1], dxs[-1], dys[-1], np.ones_like(xs[-1])])
+    sample_rays = np.dot(detector_to_sample, detector_rays)
 
     # Unpack the sample and detector ray coordinates
     sample_rays_x = sample_rays[0]
@@ -226,7 +220,6 @@ def get_ray_coords_at_scan_and_det(model, scan_pos, total_transfer_matrix, trans
     return sample_rays_x, sample_rays_y, det_rays_x, det_rays_y
 
 
-@jax.jit
 def project_frame_forward(model, detector_frame, sample_interpolant, scan_pos):
     # Return all the transfer matrices necessary for us to propagate rays through the system
     transfer_matrices, total_transfer_matrix, detector_to_sample = get_transfer_matrices(model, scan_pos)
@@ -244,13 +237,11 @@ def project_frame_forward(model, detector_frame, sample_interpolant, scan_pos):
         Detector, sample_interpolant, sample_rays_x, sample_rays_y, det_rays_x, det_rays_y
     )
     
-    # Use JAX's index update to avoid in-place mutation
-    detector_frame = detector_frame.at[det_y_px, det_x_px].set(sample_intensity)
+    detector_frame[det_y_px, det_x_px] = sample_intensity
 
     return detector_frame
 
 
-@jax.jit
 def project_frame_backward(model, detector_frame, scan_pos):
     # Return all the transfer matrices necessary for us to propagate rays through the system
     transfer_matrices, total_transfer_matrix, detector_to_sample = get_transfer_matrices(model, scan_pos)
@@ -267,8 +258,7 @@ def project_frame_backward(model, detector_frame, scan_pos):
         ScanGrid, Detector, detector_frame, sample_rays_x, sample_rays_y, det_rays_x, det_rays_y
     )
 
-    # Sum the detector intensity values at the scan pixel coordinates using JAX indexing updates
-    shifted_sum = jnp.zeros(ScanGrid.scan_shape, dtype=jnp.complex64)
-    shifted_sum = shifted_sum.at[sample_y_px, sample_x_px].add(detector_intensity)
+    shifted_sum = np.zeros(ScanGrid.scan_shape, dtype=np.complex64)
+    np.add.at(shifted_sum, (sample_y_px, sample_x_px), detector_intensity)
 
     return shifted_sum
