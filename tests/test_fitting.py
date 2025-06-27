31b2f33ba8e4cfe7048d0fd01259eb66c0d8297d
DavidLanders95 <davidlanders2009@hotmail.com>
2025-06-27 18:00:39 +0200

    Fitting for descan error works as a test

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -15,15 +15,21 @@ from libertem.udf.sum import SumUDF
 from libertem.udf.com import CoMUDF
 
 
-def descan_error_scale():
-    # Example descan error parameters
+def descan_error_params_random():
+    # Randomize descan error parameters
     return DescannerErrorParameters(
-        pxo_pxi=1.0, pxo_pyi=0.0,
-        sxo_pxi=0.0, sxo_pyi=0.0,
-        offpxi=0.0, offsxi=0.0,
-        pyo_pxi=0.0, pyo_pyi=1.0,
-        syo_pxi=0.0, syo_pyi=0.0,
-        offpyi=0.0, offsyi=0.0,
+        pxo_pxi=np.random.uniform(-2.0, 2.0),
+        pxo_pyi=np.random.uniform(-2.0, 2.0),
+        pyo_pxi=np.random.uniform(-2.0, -2.0),
+        pyo_pyi=np.random.uniform(-2.0, 2.0),
+        sxo_pxi=np.random.uniform(-2.0, 2.0),
+        sxo_pyi=np.random.uniform(-2.0, 2.0),
+        syo_pxi=np.random.uniform(-2.0, 2.0),
+        syo_pyi=np.random.uniform(-2.0, 2.0),
+        offpxi=np.random.uniform(-0.04, 0.04),
+        offsxi=np.random.uniform(-0.04, 0.04),
+        offpyi=np.random.uniform(-0.04, 0.04),
+        offsyi=np.random.uniform(-0.04, 0.04)
     )
 
 
@@ -75,82 +81,98 @@ def test_fit_descan_error_matrix_perfect_data():
         assert np.isclose(rec, exp, atol=atol), f"{field}: {rec} vs {exp}"
 
 
-# def test_fit_descan_error_generated_data(camera_lengths=(0.5, 1.0, 1.5)):
-#     # That that we can fit the descan error parameters
-#     # generated from propagating a single pixel with
-#     # descan error.
+def test_fit_descan_error_matrix_random():
 
-#     ctx = lt.Context.make_with("inline")
+    ctx = lt.Context.make_with("inline")
 
-#     grid_shape = (12, 12)
-#     scan_step = (0.01, 0.01)
-#     det_px_size = (0.01, 0.01)
+    scan_shape = (41, 41)
+    det_shape = (41, 41)
+    scan_step = (0.0005, 0.0005)
+    det_px_size = (0.01, 0.01)
 
-#     test_image = np.zeros(grid_shape, dtype=np.uint8)
-#     test_image[0, 0] = 1
+    test_image = np.ones(scan_shape, dtype=np.uint8)
 
-#     descan_error = descan_error_scale()
+    descan_error = descan_error_params_random()
 
-#     params = ModelParameters(
-#         semi_conv=1e-4,
-#         defocus=0.0,
-#         camera_length=0.5,
-#         scan_shape=grid_shape,
-#         det_shape=grid_shape,
-#         scan_step=scan_step,
-#         det_px_size=det_px_size,
-#         scan_rotation=0.0,
-#         descan_error=descan_error,
-#     )
+    params = ModelParameters(
+        semi_conv=1e-4,
+        defocus=0.0,
+        camera_length=0.5,
+        scan_shape=scan_shape,
+        det_shape=det_shape,
+        scan_step=scan_step,
+        det_px_size=det_px_size,
+        scan_rotation=0.0,
+        descan_error=descan_error,
+        flip_y=False,
+    )
 
-#     datasets = {}
-#     clengths = camera_lengths
-#     with ProcessPoolExecutor(max_workers=3) as p:
-#         futures = []
-#         for cl in clengths:
-#             _params = copy.deepcopy(params)
-#             _params["camera_length"] = cl
-#             f = p.submit(generate_dataset_from_image, _params, test_image, method="linear",
-#                          sample_scale=1.0, progress=False)
+    datasets = {}
+    clengths = (0.5, 1.0, 1.5)
+    with ProcessPoolExecutor(max_workers=3) as p:
+        futures = []
+        for cl in clengths:
+            _params = copy.deepcopy(params)
+            _params["camera_length"] = cl
+            f = p.submit(generate_dataset_from_image,
+                        _params,
+                        test_image,
+                        method="linear",
+                        sample_scale=1.0,
+                        progress=False)
+            futures.append(f)
 
-#             futures.append(f)
+        for f, cl in zip(futures, clengths):
+            data = f.result()
+            datasets[cl] = ctx.load("memory", data=data, num_partitions=1)
 
-#         for f, cl in zip(futures, clengths):
-#             data = f.result()
-#             datasets[cl] = data
+    com_r = {}
+    com_udf = CoMUDF.with_params()
+    for i, (cl, ds) in enumerate(datasets.items()):
+        sum_res, com_res = ctx.run_udf(ds, [SumUDF(), com_udf])
+        com_r[cl] = com_res
 
-#     com_r = {}
-#     com_udf = CoMUDF.with_params()
-#     for i, (cl, ds) in enumerate(datasets.items()):
-#         sum_res, com_res = ctx.run_udf(ds, [SumUDF(), com_udf])
-#         com_r[cl] = com_res
+    scan_coords = []
+    det_coords = []
+    b_vals = []
+    for camera_length, ds in datasets.items():
+        ScanGrid = comp.ScanGrid(
+            z=jnp.array(params['defocus']),
+            scan_step=params['scan_step'],
+            scan_shape=ds.shape.nav.to_tuple(),
+            scan_rotation=params['scan_rotation'],
+        )
+        scan_coords.append(ScanGrid.coords)
+        Detector = comp.Detector(
+            z=jnp.array(camera_length),
+            det_shape=ds.shape.sig.to_tuple(),
+            det_pixel_size=params['det_px_size'],
+            flip_y=params['flip_y'],
+        )
+        yx_px_det = com_r[camera_length]["raw_com"].data.reshape(-1, 2)
+        det_coords.append(np.stack(Detector.pixels_to_metres(yx_px_det.T), axis=1))
 
-#     scan_coords = []
-#     det_coords = []
-#     b_vals = []
-#     for camera_length, ds in datasets.items():
-#         ScanGrid = comp.ScanGrid(
-#             z=jnp.array(params['defocus']),
-#             scan_step=params['scan_step'],
-#             scan_shape=ds.shape.nav.to_tuple(),
-#             scan_rotation=params['scan_rotation'],
-#         )
-#         scan_coords.append(ScanGrid.coords)
-#         Detector = comp.Detector(
-#             z=jnp.array(camera_length),
-#             det_shape=ds.shape.sig.to_tuple(),
-#             det_pixel_size=params['det_px_size'],
-#             flip_y=params['flip_y'],
-#         )
-#         yx_px_det = com_r[camera_length]["raw_com"].data.reshape(-1, 2)
-#         det_coords.append(np.stack(Detector.pixels_to_metres(yx_px_det.T), axis=1))
-#         b_vals.append(camera_length - params['defocus'])
+        b_vals.append(camera_length - params['defocus'])
 
-#     bvals = np.concatenate(
-#         tuple(np.full((c.shape[0],), b) for b, c in zip(b_vals, scan_coords))
-#     )
-#     scan_coords = np.concatenate(scan_coords, axis=0)
-#     det_coords = np.concatenate(det_coords, axis=0)
-#     bvals.shape
+    bvals = np.concatenate(
+        tuple(np.full((c.shape[0],), b) for b, c in zip(b_vals, scan_coords))
+    )
+    scan_coords = np.concatenate(scan_coords, axis=0)
+    det_coords = np.concatenate(det_coords, axis=0)
 
-#     print(det_coords)
+    mask = ~(np.all(det_coords == 0.0, axis=1))
+    bvals = bvals[mask]
+    scan_coords = scan_coords[mask]
+    det_coords = det_coords[mask]
+
+    err = fit_descan_error_matrix(scan_coords, det_coords, bvals,
+                                num_samples=len(bvals))
+
+    # check x-related fields
+    for key in err._fields:
+        fitted_val = getattr(err, key)
+        known_val = getattr(descan_error, key)
+        np.testing.assert_allclose(
+            fitted_val, known_val, atol=1e-1,
+            err_msg=f"Field {key} does not match: {fitted_val} vs {known_val}"
+        )
