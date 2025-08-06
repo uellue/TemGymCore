3f733167c7266461d8e020df2fdc32bb351028e1
DavidLanders95 <davidlanders2009@hotmail.com>
2025-08-06 13:32:23 +0200

    Edited udf to only solve model once, and to use the transfer matrices and their derivatives to calculate the transfer matrix for each scan position

--- src/microscope_calibration/udf.py
+++ src/microscope_calibration/udf.py
@@ -3,7 +3,7 @@ from libertem.udf import UDF
 from libertem.common.buffers import AuxBufferWrapper
 
 from .model import ModelParameters, create_stem_model
-from .stemoverfocus import project_coordinates_backward, inplace_sum
+from .stemoverfocus import project_coordinates_backward, inplace_sum, solve_model_fourdstem_wrapper
 
 
 class ShiftedSumUDF(UDF):
@@ -20,8 +20,13 @@ class ShiftedSumUDF(UDF):
         model = create_stem_model(params_dict)
         scan_coords = model.scan_grid.coords
         detector_coords = model.detector.coords
+
+        total_tm_and_grad, scangrid_tm_and_grad = solve_model_fourdstem_wrapper(model)
+
         return {
             "model": model,
+            "total_tm_and_grad": total_tm_and_grad,
+            "scangrid_tm_and_grad": scangrid_tm_and_grad,
             "scan_coords": scan_coords,
             "detector_coords": detector_coords,
         }
@@ -47,11 +52,16 @@ class ShiftedSumUDF(UDF):
             self.meta.coordinates.ravel(),
             self.meta.dataset_shape.nav,
         )
+        model = self.task_data.model
+        total_tm_and_grad = self.task_data.total_tm_and_grad
+        scangrid_tm_and_grad = self.task_data.scangrid_tm_and_grad
         det_coords = self.task_data.detector_coords
         scan_pos = self.task_data.scan_coords[scan_pos_flat]
-        model = self.task_data.model
+
         px_y, px_x, mask = project_coordinates_backward(
             model,
+            total_tm_and_grad,
+            scangrid_tm_and_grad,
             det_coords,
             scan_pos,
         )
